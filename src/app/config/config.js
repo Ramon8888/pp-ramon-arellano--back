65b34e840afb7148204d04e76eaedaa4b99220b7
Ramon Arellano <rarellano8888@gmail.com>
2021-03-22 07:51:33 -0600

    Upgrade expiration token time

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     // Security config (jwt)
     jwtKey: 'This_is_my_secret_key_compa', // Secret key
-    timeExp: 60 * 3 //time expiration
+    timeExp: (60 * 60 * 24) * 14 //time expiration
 }
