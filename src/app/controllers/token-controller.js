0fd456c99228f06d4ee4f6f79a433bfecba8dbba
Ramon Arellano <rarellano8888@gmail.com>
2021-03-22 07:51:05 -0600

    Delete test lines

--- src/app/controllers/token-controller.js
+++ src/app/controllers/token-controller.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const parameters = require('../config/config');
 
-console.log(parameters.jwtKey);
-
 // Token generator sevice
 router.get('/token-generator', async (req, res) => {
     const token = jwt.sign({auth: true}, parameters.jwtKey, {
